e2b2907267a44fa0a21427e2471c55aadd7d2228
laraferreri <lbf287@nyu.edu>
2020-11-30 16:52:21 -0500

    aPI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
 import "./App.css";
 import Login from "./containers/Login";
 import CreateAccount from "./containers/CreateAccount";
 import UserProfile from "./containers/UserProfile";
 import Header from "./components/Header";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: "exercise5-cd2f7.firebaseapp.com",
+  databaseURL: "https://exercise5-cd2f7.firebaseio.com",
+  projectId: "exercise5-cd2f7",
+  storageBucket: "exercise5-cd2f7.appspot.com",
+  messagingSenderId: "547318276189",
+  appId: "1:547318276189:web:bff95e8bfd875eaee16db4",
+};
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="Header">
       <nav>
         <a href="/">User Profile</a>
         <a href="/Login">Login</a>
         <a href="/logout">Logout</a>
-        <a href="/create-account">Create Account</a>
+        <a href="/CreateAccount">Create Account</a>
       </nav>
     </header>
   );
